a36f569f67f8b7a27f5b05619765716cdc2ec7fd
TabajaraKrausburg <tabajararodrigues@yahoo.com.br>
2017-05-08 16:23:40 -0300

    Now, it is only one team per match ("server.json").
    The integrated execution server/jacamo is running on
    "ScenarioRunServer". It is just run as "JUnit Test"

--- test/pucrs/agentcontest2017/ScenarioRunServer.java
+++ test/pucrs/agentcontest2017/ScenarioRunServer.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import jacamo.infra.JaCaMoLauncher;
 import jason.JasonException;
+import massim.Server;
 
 
 public class ScenarioRunServer {
@@ -18,23 +19,20 @@ public class ScenarioRunServer {
 			@Override
 			public void run() {
 				try {
-					Runtime.getRuntime().exec("java -jar server-2017-0.2-jar-with-dependencies.jar --monitor");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					Server.main(new String[] {"-conf", "conf/SampleConfig.json", "--monitor"});
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}).start();
 
-//		try {
-//			JaCaMoLauncher runner = new JaCaMoLauncher();
-//			runner.init(new String[] { "test/pucrs/agentcontest2017/scenario.jcm" });
-//			runner.getProject().addSourcePath("./src/pucrs/agentcontest2017/agt");
-//			runner.create();
-//			runner.finish();
-//		} catch (JasonException e) {
-//			e.printStackTrace();
-//		}
+		try {			
+			System.out.println("FEITO");
+			JaCaMoLauncher.main(new String[] { "test/pucrs/agentcontest2017/scenario.jcm"});
+		} catch (JasonException e) {
+			System.out.println("Exception: "+e.getMessage());
+			e.printStackTrace();
+		}
 
 	}
 
